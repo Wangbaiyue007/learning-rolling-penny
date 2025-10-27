a73fec6decf4963cabb3f34530ef0ccfa87fc1bd
baiyue <baiyuew@umich.edu>
2025-10-26 21:57:26 -0400

    save weights; add plot function

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from motion import InfGenerator
+from plot import plot
 import torch
 from torchdiffeq import odeint
 import numpy as np
@@ -11,12 +12,11 @@ def makedirs(dirname):
         os.makedirs(dirname)
 
 makedirs('png')
+makedirs('figs')
+makedirs('weights')
 fig = plt.figure(figsize=(12, 4), facecolor='white')
 ax_traj = fig.add_subplot(121, frameon=False)
 ax_A = fig.add_subplot(122, frameon=False)
-# ax_phase = fig.add_subplot(132, frameon=False)
-# ax_vecfield = fig.add_subplot(133, frameon=False)
-plt.show(block=False)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.set_default_device('cuda:0')
@@ -25,20 +25,20 @@ t = torch.arange(0., 20, 0.01).to(device)
 opts = {
     'data_size': t.size(0),
     'batch_time': 20,
-    'batch_size': 50
+    'batch_size': 100
 }
 class Sine(torch.nn.Module):
     def forward(self, x):
         return torch.sin(x)
     
 network = torch.nn.Sequential(
-            torch.nn.Linear(4, 10),
+            torch.nn.Linear(4, 20),
             Sine(),
-            torch.nn.Linear(10, 10),
+            torch.nn.Linear(20, 20),
             Sine(),
-            torch.nn.Linear(10, 10),
+            torch.nn.Linear(20, 20),
             torch.nn.ELU(alpha=0.5),
-            torch.nn.Linear(10, 3)
+            torch.nn.Linear(20, 3)
         ).to(device)
 for m in network.modules():
     if isinstance(m, torch.nn.Linear):
@@ -52,67 +52,24 @@ def get_batch(true_y, options):
     batch_y = torch.stack([true_y[s + i] for i in range(options['batch_time'])], dim=0)  # (T, M, D)
     return batch_y0.to(device), batch_t.to(device), batch_y.to(device), s
 
-def visualize(true_p, pred_p, true_A, pred_A, odefunc, itr):
-
-    ax_traj.cla()
-    ax_traj.set_title('p1 to p4 vs t')
-    ax_traj.set_xlabel('t')
-    ax_traj.set_ylabel('p')
-    ax_traj.plot(t.cpu().numpy(), true_p.cpu().numpy()[:, 0], color='grey', linestyle='-')
-    ax_traj.plot(t.cpu().numpy(), true_p.cpu().numpy()[:, 1], color='grey', linestyle='-')
-    ax_traj.plot(t.cpu().numpy(), true_p.cpu().numpy()[:, 2], color='grey', linestyle='-')
-    ax_traj.plot(t.cpu().numpy(), true_p.cpu().numpy()[:, 3], color='grey', linestyle='-')
-    ax_traj.plot(t.cpu().numpy(), pred_p.cpu().numpy()[:, 0], color='black', linestyle='--')
-    ax_traj.plot(t.cpu().numpy(), pred_p.cpu().numpy()[:, 1], color='black', linestyle='--')
-    ax_traj.plot(t.cpu().numpy(), pred_p.cpu().numpy()[:, 2], color='black', linestyle='--')
-    ax_traj.plot(t.cpu().numpy(), pred_p.cpu().numpy()[:, 3], color='black', linestyle='--')
-    ax_traj.set_xlim(t.cpu().min(), t.cpu().max())
-    # ax_traj.set_ylim(-2, 2)
-    # ax_traj.legend()
-
-    ax_A.cla()
-    ax_A.set_title('A1 to A3 vs t')
-    ax_A.set_xlabel('t')
-    ax_A.set_ylabel('A')
-    ax_A.plot(t.cpu().numpy(), true_A.cpu().numpy()[:, 0], 'g-')
-    ax_A.plot(t.cpu().numpy(), true_A.cpu().numpy()[:, 1], 'b-')
-    ax_A.plot(t.cpu().numpy(), true_A.cpu().numpy()[:, 2], 'r-')
-    ax_A.plot(t.cpu().numpy(), pred_A.cpu().numpy()[:, 0], color='green', linestyle='--')
-    ax_A.plot(t.cpu().numpy(), pred_A.cpu().numpy()[:, 1], color='blue', linestyle='--')
-    ax_A.plot(t.cpu().numpy(), pred_A.cpu().numpy()[:, 2], color='red', linestyle='--')
-    ax_A.set_xlim(t.cpu().min(), t.cpu().max())
-    # ax_A.legend()
-
-    fig.tight_layout()
-    plt.savefig('png/{:03d}'.format(itr))
-    plt.draw()
-    plt.pause(0.1)
-
 if __name__ == "__main__":
     dynamics_true = InfGenerator().to(device)
     dynamics_param = InfGenerator(nn=network).to(device)
     optimizer = torch.optim.RMSprop(dynamics_param.parameters(), lr=1e-3)
+    torch.save(dynamics_param.state_dict(), 'weights/not_trained_model_{}.pth'.format(datetime.now().strftime("%m_%d-%H:%M:%S")))
 
-    # with torch.no_grad():
     p0 = dynamics_true.sys.p()
     true_p = odeint(dynamics_true, p0, t).to(device)
-    true_p_dot = torch.zeros((t.size(0), 8)).to(device)
     true_A = torch.zeros((t.size(0), 3)).to(device)
-    pred_p = torch.zeros((t.size(0), 4)).to(device)
     for i in range(t.size(0)):
         dynamics_true.sys.evaluate(true_p[i])
-        pred_p[i] = dynamics_true.sys.p()[:4]
-        true_p_dot[i] = dynamics_true.sys.p_dot(pred_p[i])
         true_A[i] = - dynamics_true.sys.u1[1:4]
-    # visualize(true_p, pred_p, true_A, true_A, dynamics_param, 0)
 
     for itr in range(0, 2001):
         optimizer.zero_grad()
         batch_p0, batch_t, batch_p, s = get_batch(true_p, opts)
-        batch_p_dot = true_p_dot[s]
         pred_p = odeint(dynamics_param, batch_p0, batch_t).to(device)
-        # pred_A = dynamics_param.net(true_p[:, 4:]).to(device)
-        loss = torch.sum(torch.square(pred_p - batch_p)) #+ 1e-3 * torch.sum(torch.square(pred_A))
+        loss = torch.sum(torch.square(pred_p - batch_p))
         loss.backward()
         optimizer.step()
         print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
@@ -121,7 +78,7 @@ if __name__ == "__main__":
             with torch.no_grad():
                 pred_p = odeint(dynamics_param, p0, t)
                 pred_A = dynamics_param.net(true_p[:, 4:]).to(device)
-                visualize(true_p, pred_p, true_A, pred_A, dynamics_param, itr)
+                plot(true_p, pred_p, true_A, pred_A, ax_traj, ax_A, fig, t, itr, True)
         
     # save trained model
-    torch.save(dynamics_param.state_dict(), 'weights/trained_model_{}.pth'.format(datetime.now().strftime("%D:%H:%M:%S")))
+    torch.save(dynamics_param.state_dict(), 'weights/trained_model_{}.pth'.format(datetime.now().strftime("%m_%d-%H:%M:%S")))
